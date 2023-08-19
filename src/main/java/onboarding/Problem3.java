cbe4d2c56084fec5fe8ed229bea8eea4a9d0d4cc
Hae-K <bhk4392@inha.edu>
2023-08-19 21:11:30 +0900

    문제3 풀이

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            int num=i;
+            while(num>0){
+                int cmp = num%10;
+                if(cmp%3==0 && cmp!=0){
+                    answer++;
+                }
+                num/=10;
+            }
+        }
         return answer;
     }
 }
